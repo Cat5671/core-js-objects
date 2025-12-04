0e85a485c244c94dfca4f3bd014164389c054fc7
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-12-04 12:36:15 +0300

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  const newObj = {};
+  return Object.assign(newObj, obj);
 }
 
 /**
@@ -32,10 +33,16 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
-}
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (!acc[key]) acc[key] = value;
+      else acc[key] += obj[key];
+    });
 
+    return acc;
+  }, {});
+}
 /**
  * Removes a properties from an object.
  *
@@ -49,8 +56,13 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = { ...obj };
+
+  for (let i = 0; i < keys.length; i += 1) {
+    delete newObj[keys[i]];
+  }
+  return newObj;
 }
 
 /**
@@ -65,8 +77,13 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const entriesObj1 = Object.entries(obj1);
+  const entriesObj2 = Object.entries(obj2);
+
+  if (entriesObj1.length !== entriesObj2.length) return false;
+
+  return entriesObj1.every(([key, value]) => obj2[key] === value);
 }
 
 /**
