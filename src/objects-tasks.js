c0b3b6b37d23d12f0157130ffc951ea19d8258a3
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2025-12-04 16:10:36 +0300

    feat: add new solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -149,8 +149,26 @@ function makeWord(/* lettersObject */) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  const money = {
+    25: 0,
+    50: 0,
+  };
+  let el;
+  for (let i = 0; i < queue.length; i += 1) {
+    el = queue[i];
+    if (el === 25) money['25'] += 1;
+    else if (el === 50 && money['25'] !== 0) {
+      money['25'] -= 1;
+      money['50'] += 1;
+    } else if (el === 100 && money['50'] !== 0 && money['25'] !== 0) {
+      money['50'] -= 1;
+      money['25'] -= 1;
+    } else if (el === 100 && money['25'] > 2) {
+      money['25'] -= 3;
+    } else return false;
+  }
+  return true;
 }
 
 /**
@@ -166,8 +184,12 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea: () => width * height,
+  };
 }
 
 /**
@@ -180,8 +202,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { height: 10, width: 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -195,8 +217,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const parseJSON = JSON.parse(json);
+  Object.setPrototypeOf(parseJSON, proto);
+  return parseJSON;
 }
 
 /**
